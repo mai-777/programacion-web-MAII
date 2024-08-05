c8f4c53a71e63a7c99bfa9a25f3f3e949603617e
Mailen Escobar <mailenescobar5d@gmail.com>
2024-08-05 17:10:19 -0300

    creamos un projecto en react-vite en el cual vamos a hacer nuestros portfolios

---
+++ React/portfolio-project/src/components/Intro/Intro.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import "./Intro.css";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Andrew Thomas</span>
+          <span>
+            Frontend Developer with high level of experience in web designing
+            and development, producting the Quality work
+          </span>
+        </div>
+        <Link to="contact" smooth={true} spy={true}>
+          <button className="button i-button">Hire me</button>
+        </Link>
+        {/* social icons */}
+        <div className="i-icons"></div>
+      </div>
+      {/* right image side */}
+      <div className="i-right">
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
